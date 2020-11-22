6dc009254e0f2dd9b2fbe43ab1a5525061fdf0fb
Kevin <kevin.kaftan@hs-furtwangen.de>
2020-11-22 07:35:28 +0100

    y

---
+++ src/main/java/de/hfu/main.java
@@ -0,0 +1,13 @@
+package de.hfu;
+
+import java.util.Scanner;
+import java.io.*;
+
+
+public class main{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input.toUpperCase());
+    }
+}
