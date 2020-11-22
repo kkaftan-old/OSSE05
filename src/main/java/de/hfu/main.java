ef2e0cfa40feeeea9fda657037e66973443a9589
Kevin <kevin.kaftan@hs-furtwangen.de>
2020-11-22 23:55:36 +0100

    eclipse run

--- src/main/java/de/hfu/main.java
+++ src/main/java/de/hfu/main.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 import java.io.*;
 
 
+/**
+ * @author kevin
+ * @email kevin.kaftan@hs-furtwangen.de
+ */
 public class main{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
